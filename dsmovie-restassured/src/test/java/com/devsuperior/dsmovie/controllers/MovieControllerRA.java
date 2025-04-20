3fed374eb41b45c0712f2c1071cd6e6e83236922
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-04-20 15:40:28 -0300

    UPDATE MovieControllerRA - all requested tests done

--- dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -5,9 +5,11 @@ import static io.restassured.matcher.RestAssuredMatchers.*;
 
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.json.JSONException;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -20,52 +22,122 @@ public class MovieControllerRA {
 	private String clientUsername, clientPassword, adminUsername, adminPassword;
 	private String adminToken, clientToken, invalidToken;
 	private Long existingMovieId, nonExistingMovieId, dependentMovieId;
-	private String movieName;
+	private String movieTitle;
 
 	private Map<String, Object> postMovieInstance;
-	private Map<String, Object> putMovieInstance;
 
 	@BeforeEach
 	public void setup() throws JSONException {
 		baseURI = "http://localhost:8080";
 		
-		clientUsername = "maria@gmail.com";
+		clientUsername = "alex@gmail.com";
 		clientPassword = "123456";
-		adminUsername = "alex@gmail.com";
+		adminUsername = "maria@gmail.com";
 		adminPassword = "123456";
 		
 		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
 		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
 		invalidToken = adminToken + "xyz";
 		
-		movieName = "";
+		movieTitle = "Star Wars";
+		existingMovieId = 3L;
+		nonExistingMovieId = 100L;
+		dependentMovieId = 1L;
+
+		postMovieInstance = new HashMap<>();
+		postMovieInstance.put("title", "Solo Leveling");
+		postMovieInstance.put("score", 0.0);
+		postMovieInstance.put("count", 0);
+		postMovieInstance.put("image", "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/geCRueV3ElhRTr0xtJuEWJt6dJ1.jpg");
 	}
 	
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		given()
+			.get("/movies?page=0")
+		.then()
+			.statusCode(200)
+			.body("content.title", hasItems("Thor", "Titanic"));
 	}
 	
 	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+		given()
+			.get("/movies?title={movieTitle}", movieTitle)
+		.then()
+			.statusCode(200)
+			.body("content.id[0]", is(10))
+			.body("content.title[0]", equalTo("Rogue One: Uma História Star Wars"))
+			.body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/6t8ES1d12OzWyCGxBeDYLHoaDrT.jpg"));
+
 	}
 	
 	@Test
-	public void findByIdShouldReturnMovieWhenIdExists() {		
+	public void findByIdShouldReturnMovieWhenIdExists() {
+		given()
+			.get("/movies/{id}", existingMovieId)
+		.then()
+			.statusCode(200)
+			.body("id", is(3))
+			.body("title", equalTo("O Espetacular Homem-Aranha 2: A Ameaça de Electro"))
+			.body("image", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/u7SeO6Y42P7VCTWLhpnL96cyOqd.jpg"));
 	}
 	
 	@Test
 	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
+		given()
+			.get("/movies/{id}", nonExistingMovieId)
+		.then()
+			.statusCode(404);
 	}
 	
 	@Test
-	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
+	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {
+		postMovieInstance.put("title", null);
+		JSONObject newMovie = new JSONObject(postMovieInstance);
+
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + adminToken)
+			.body(newMovie)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/movies")
+		.then()
+			.statusCode(422)
+			.body("errors.message[0]", equalTo("Campo requerido"));
 	}
 	
 	@Test
 	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+		JSONObject newMovie = new JSONObject(postMovieInstance);
+
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + clientToken)
+			.body(newMovie)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/movies")
+		.then()
+			.statusCode(403);
 	}
 	
 	@Test
 	public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+		JSONObject newMovie = new JSONObject(postMovieInstance);
+
+		given()
+			.header("Content-type", "application/json")
+			.header("Authorization", "Bearer " + invalidToken)
+			.body(newMovie)
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post("/movies")
+		.then()
+			.statusCode(401);
 	}
 }
